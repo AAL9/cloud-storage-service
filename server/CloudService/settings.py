67ccbf2e3bdd0edebdda0670d6331a64fb5f5ed6
Abdulaziz <a.alluheeb@gmail.com>
2023-07-13 16:06:32 +0300

    fix name of project

--- server/CloudService/settings.py
+++ server/CloudService/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'cloudService.urls'
+ROOT_URLCONF = 'CloudService.urls'
 
 TEMPLATES = [
     {
@@ -59,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cloudService.wsgi.application'
+WSGI_APPLICATION = 'CloudService.wsgi.application'
 
 
 # Database
--- server/CloudService/urls.py
+++ server/CloudService/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cloudService project.
+URL configuration for CloudService project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
