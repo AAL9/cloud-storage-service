1e26306518194db4cf6bfe7229b725826dabddc1
Abdulaziz <a.alluheeb@gmail.com>
2023-07-21 17:05:52 +0300

    create FileMetaData model

--- server/base/models.py
+++ server/base/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-# Create a meta data model
+
+class FileMetaData(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField()
+    hash = models.CharField(max_length=400)
+    path = models.CharField(max_length=100)
+    size = models.PositiveBigIntegerField()
