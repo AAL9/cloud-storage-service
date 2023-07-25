65e212bd6293fd35b408ad96e872e6f8affe01d3
Abdulaziz <a.alluheeb@gmail.com>
2023-07-25 15:02:00 +0300

    add FileSerializer.

--- server/base/serializers.py
+++ server/base/serializers.py
@@ -6,8 +6,14 @@ from .models import FileMetaData
 
 
 class FileMetaDataSerializer(serializers.ModelSerializer):
-    owner = serializers.CharField(required= False)  # Add a write-only field for owner name
+    owner = serializers.CharField(
+        required=False
+    )  # Add a write-only field for owner name
 
     class Meta:
-        model= FileMetaData
-        fields= ['id','owner','name','created_at','updated_at','hash','path','size']
+        model = FileMetaData
+        fields = ["owner", "name", "updated_at", "hash", "path", "size"]
+
+
+class FileSerializer(serializers.Serializer):
+    file = serializers.FileField()
