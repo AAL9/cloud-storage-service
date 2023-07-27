74e012d13edb126d20aa97b606ac45906dc3165b
Abdulaziz <a.alluheeb@gmail.com>
2023-07-27 10:54:50 +0300

    create upload_file, delete_file, create_file

--- server/base/files_handler.py
+++ server/base/files_handler.py
@@ -19,22 +19,50 @@ def get_all_files_dirs(directory_path, file_list=None):
         file_list = [path.replace(root_dir, "") for path in file_list]
     return file_list
 
-def remove_file(owner, path):
+
+def upload_file(owner: str, path: str, file):
     storage_folder_path = os.path.abspath(
-    os.path.join(__file__, "..", "..", "..", "cloud_storage")
+        os.path.join(__file__, "..", "..", "..", "cloud_storage")
     )
-    file_path = os.path.join(
-    storage_folder_path, owner
+    file_path = os.path.join(storage_folder_path, owner)
+    file_path = file_path + path
+    dir_path = os.path.dirname(file_path)
+    os.makedirs(dir_path, exist_ok=True)
+    with open(file_path, "wb+") as destination_file:
+        for chunk in file.chunks():
+            destination_file.write(chunk)
+
+
+def update_file(owner: str, path: str, file):
+    storage_folder_path = os.path.abspath(
+        os.path.join(__file__, "..", "..", "..", "cloud_storage")
     )
-    file_path = file_path + str(path)
-            
+    file_path = os.path.join(storage_folder_path, owner)
+    file_path = file_path + path
+    dir_path = os.path.dirname(file_path)
+    os.makedirs(dir_path, exist_ok=True)
+    with open(file_path, "wb+") as destination_file:
+        for chunk in file.chunks():
+            destination_file.write(chunk)
+
+
+def delete_file(owner: str, path: str):
+    storage_folder_path = os.path.abspath(
+        os.path.join(__file__, "..", "..", "..", "cloud_storage")
+    )
+    file_path = os.path.join(storage_folder_path, owner)
+    file_path = file_path + path
+
     try:
         os.remove(file_path)
-        return {"message" :f"File '{path}' has been deleted successfully."}
+        message = f"File '{path}' has been deleted successfully."
+        return message
     except FileNotFoundError:
-        return {"message" :f"File '{path}' not found. Unable to delete."}
+        message = f"File '{path}' not found. Unable to delete."
+        return message
     except PermissionError:
-        return {"message" :f"Permission denied. Unable to delete file '{path}'."}
+        message = f"Permission denied. Unable to delete file '{path}'."
+        return message
     except Exception as e:
-        return {"message" :f"An error occurred while deleting file '{file_path}': {str(e)}"}
-
+        message = f"An error occurred while deleting file '{file_path}': {str(e)}"
+        return message
