376eb1b1fe390bb6a38dc1bff4c66142d5df75d4
Abdulaziz <a.alluheeb@gmail.com>
2023-07-31 13:59:23 +0300

    remove a not needed anymore function.

--- server/base/files_handler.py
+++ server/base/files_handler.py
@@ -1,25 +1,6 @@
 import os
 
 
-def get_all_files_dirs(directory_path, file_list=None):
-    root_dir = ""
-    if file_list is None:
-        root_dir = directory_path
-        file_list = []
-
-    for item in os.listdir(directory_path):
-        item_path = os.path.join(directory_path, item)
-        if os.path.isdir(item_path):
-            file_list.append(item_path + "/")
-            if os.listdir(item_path):
-                get_all_files_dirs(item_path, file_list)
-        else:
-            file_list.append(item_path)
-    if root_dir == directory_path:
-        file_list = [path.replace(root_dir, "") for path in file_list]
-    return file_list
-
-
 def get_file_path(owner: str, path: str):
     storage_folder_path = os.path.abspath(
         os.path.join(__file__, "..", "..", "..", "cloud_storage")
